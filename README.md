909139bfed270bfe51e81b5b20f79128a719ad8f
Liam Fruzyna <liam@fruzyna.net>
2023-03-21 09:07:33 -0700

    Updated script and readme for 2023

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # 2023_scouting_data
+
 WildRank scouting data collected during our 2023 Charged Up competitions.
+
+## What is WildRank?
+
+[WildRank](https://github.com/wildstang/wildrank) is our scouting app. 
+
+### Application Versions
+
+This repo contains data collected with WildRank 2 during the official competition season. The versions or Git hashes of the app used to collect the data can be found below. The data collect with version 2 should be compatible with any version 2 release of the app.
+
+| Event                         | Version |
+| ----------------------------- | ------- |
+| Midwest Regional              | 2.1.1   |
+| Seven Rivers Regional         | 2.2.0   |
+| Championship                  |         |
+
+## How do I read this data?
+
+The best way is with WildRank where you can upload a zip archive of the results using the "Transfer Raw Data" page. Otherwise, the data is all in JSON, easily parsable with most programming languages. The results are all stored by match-team as key-value pairs. Cycles are stored as a list of recurring pairs.
+
+## Data Integrity
+
+This is raw data from our scouters. The quantitative data has not been modified, but qualitative data has been removed. This means there are known (and unknown) errors in the data. We chose not to correct our data after the events, first because it would be too time consuming and second to offer us uncleaned data for future testing/learning purposes. If in the future this data is cleaned the cleaned data will be posted alongside the raw data here.
+
+## Thanks
+
+Thank you to all the scouters who contributed to this data. Our scouters spent countless hours in the stands and collected the vital data.
---
+++ cleanup.py
@@ -0,0 +1,46 @@
+# Script used to remove PII and opinions from data
+# Liam Fruzyna 2023-03-21
+
+import sys, os, json
+
+# check for path argument
+if len(sys.argv) < 2:
+    print('Requires path as argument')
+    exit(1)
+
+dir = sys.argv[1]
+
+# go over every json file in given directory
+for filename in os.listdir(dir):
+    path = os.path.join(dir, filename)
+    if os.path.isfile(path) and filename.endswith('.json'):
+        obj = None
+
+        # read json
+        with open(path, 'r') as f:
+            obj = json.load(f)
+
+            # replace values
+            if not isinstance(obj, list):
+                # remove school and grad year from student IDs
+                if 'meta_scouter_id' in obj.keys():
+                    obj['meta_scouter_id'] -= obj['meta_scouter_id'] // 1000 * 1000
+                if 'meta_note_scouter_id' in obj.keys():
+                    obj['meta_note_scouter_id'] -= obj['meta_note_scouter_id'] // 1000 * 1000
+                # remove ratings
+                if 'match_tele_driver_driver_skill' in obj.keys():
+                    obj['match_tele_driver_driver_skill'] = 0
+                if 'pit_pit_other_build_quality' in obj.keys():
+                    obj['pit_pit_other_build_quality'] = 0
+                # remove notes
+                if 'match_tele_post_match_notes' in obj.keys():
+                    obj['match_tele_post_match_notes'] = 'Removed'
+                if 'note_notes_team_notes' in obj.keys():
+                    obj['note_notes_team_notes'] = 'Removed'
+                if 'pit_pit_other_notes' in obj.keys():
+                    obj['pit_pit_other_notes'] = 'Removed'
+
+        # rewrite json
+        with open(path, 'w') as f:
+            if obj is not None:
+                f.write(json.dumps(obj))
